0d69ce2cb38c03c3d28436aa4701700a51a4a72f
BiwashGurung <varungrg10@gmail.com>
2024-08-05 17:20:05 +0545

    Case Sensitivity

---
+++ Case Sensitivity.php
@@ -0,0 +1,15 @@
+<!DOCTYPE Html>
+<html>
+    <head>
+        <title>Case Sensitivity</title>
+    </head>
+    <body>
+        <h1>Trying Case Sensitivity of echo</h1>
+        <?php
+        ECHO"Hello World 1 <br>";
+        Echo"HelloWorld 2 <br>";
+        echo"Helloworld 3<br>";
+        ecHo"Helloworld 4";
+        ?>
+    </body>
+</html>
