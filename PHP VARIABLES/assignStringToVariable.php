846d98b9b3994e4c6fbbe2e2271220c9fedb84c0
BiwashGurung <varungrg10@gmail.com>
2024-08-06 17:03:29 +0545

    assign String

---
+++ PHP VARIABLES/assignStringToVariable.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Assign String to a Variable
+Assigning a string to a variable is done with the variable name followed by an equal sign and the string:
+
+String variables can be declared either by using double or single quotes, but you should be aware of the differences.
+*/
+$x = 'Dexy';
+echo $x;
+
+
+//Yo ma chai k va vanda dutai variable ko name same huda chai mathi ko(purano variable) ho vaney (talako new variable ho)jasla garda same variable name vayo vaney new one chai updated huncha
+$x = 'Dexy';
+$x = "DExy";
+echo $x;
+
+
+?>
