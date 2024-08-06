c2f85ccff44efb6f6e0b8540428540abfde18559
BiwashGurung <varungrg10@gmail.com>
2024-08-06 17:17:08 +0545

    feat: php variable scope

---
+++ PHP VARIABLES/phpVariableScope.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>PHP Variables Scope</h1>
+<p>In PHP, variables can be declared anywhere in the script.
+
+The scope of a variable is the part of the script where the variable can be referenced/used.
+
+PHP has three different variable scopes:
+
+local
+global
+static </p> 
+</body>
+</html>
