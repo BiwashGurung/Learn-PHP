222b0be01018e506d36de0b392c3a5bceb7c7e31
BiwashGurung <varungrg10@gmail.com>
2024-08-06 16:57:25 +0545

    feat:getdatatype

---
+++ PHP VARIABLES/getTheType.php
@@ -0,0 +1,17 @@
+<?php
+//To get the data type of a variable, use the var_dump() function.
+$x = 1;
+$y = "qqq";
+$z = 8.7;
+var_dump($x , $y , $z);
+//Seeing what var_dump() returns for other data types:
+var_dump(5);
+var_dump("dexx");
+var_dump(3.1);
+var_dump(true);
+var_dump([2,1,22]);
+var_dump(NULL);
+
+
+
+?>
