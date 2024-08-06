c696f099ee70df8925df5e8c8008b9ff51c12ed1
BiwashGurung <varungrg10@gmail.com>
2024-08-06 16:40:13 +0545

    feat: outputVariable

--- PHP VARIABLES/outputVariables.php
+++ PHP VARIABLES/outputVariables.php
@@ -1,4 +1,12 @@
 <?php
 
+//The PHP "echo" statement is often used to output data to the screen.
+$txt = "We love you dexy";
+echo"my fav color is " . $txt;
+Print "I love " . $txt . "!";
 
+//The following example will output the sum of two variables:
+$x = 5;
+$y = 4;
+echo $x + $y; 
 ?>
