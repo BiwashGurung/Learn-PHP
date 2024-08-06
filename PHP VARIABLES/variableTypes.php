9bfa57492cee3c86f5adfae449435fe5de7821b6
BiwashGurung <varungrg10@gmail.com>
2024-08-06 16:45:48 +0545

    feat: variable types

---
+++ PHP VARIABLES/variableTypes.php
@@ -0,0 +1,19 @@
+<?php
+//PHP has no command for declaring a variable, and the data type depends on the value of the variable.
+$x = 07;// its a integer
+$y = "Dexy";// Its a string
+echo $x;
+echo $y;
+
+/*
+PHP supports the following data types:
+
+String
+Integer
+Float (floating point numbers - also called double)
+Boolean
+Array
+Object
+NULL
+Resource */
+?>
