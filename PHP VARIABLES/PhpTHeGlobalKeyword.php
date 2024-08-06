6c24b1710660fade71ea6449f3dc8e0f7a44a63f
BiwashGurung <varungrg10@gmail.com>
2024-08-06 18:04:41 +0545

    feat; global keyword

---
+++ PHP VARIABLES/PhpTHeGlobalKeyword.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+/*The global keyword is used to access a global variable from within a function.
+
+To do this, use the global keyword before the variables (inside the function): */
+$x = 12;
+$y = 11;
+
+function test(){
+    global $x , $y;
+    $y = $x + $y;
+}
+test();
+echo $y;
+
+?>
+
+</body>
+</html>
