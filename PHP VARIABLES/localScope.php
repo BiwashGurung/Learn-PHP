e8a6a8db9d4599d877df5946ede78de0ccf73a92
BiwashGurung <varungrg10@gmail.com>
2024-08-06 17:42:10 +0545

    feat: localscope

--- PHP VARIABLES/localScope.php
+++ PHP VARIABLES/localScope.php
@@ -1,8 +1,18 @@
 <!DOCTYPE html>
 <html>
 <body>
-
+<h1>A variable declared within a function has a LOCAL SCOPE and can only be accessed within that function:</h1>
+<p>Example<br>
+    Variable with local scope:</p>
 <?php
+function localScope(){
+    $x=1111;
+    // variable ra local scope use gareko
+    echo"This is inside a local scope $x";
+}
+localScope();
+//function vitra ko variable lai call garda error aaunca ki natra value dekhaudaina . based on your system
+echo"This is outside a local scope $x";
 
 ?>
 </body>
