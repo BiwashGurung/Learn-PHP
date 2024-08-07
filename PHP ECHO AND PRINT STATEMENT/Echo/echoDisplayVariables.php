3613619eed45ddc2adcda9edc6172840d7e0f9a5
BiwashGurung <varungrg10@gmail.com>
2024-08-07 12:56:45 +0545

    feat:echo display variables

--- PHP ECHO AND PRINT STATEMENT/Echo/echoDisplayVariables.php
+++ PHP ECHO AND PRINT STATEMENT/Echo/echoDisplayVariables.php
@@ -1,8 +1,13 @@
 <!DOCTYPE html>
 <html>
     <body>
+        <p>The following example shows how to output text and variables with the echo statement:</p>
        <?php
-       
+       $txt1 = "Hey there! I am learning PHP";
+       $txt2 = "W3school";
+       echo"<h2>$txt1</h2>";
+       echo"IF you also wanna learn it then search it on your suitable browser $txt2";
+
        ?> 
     </body>
 </html>
