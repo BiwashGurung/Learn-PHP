6c809a06d039160c398cb101082d625172400382
BiwashGurung <varungrg10@gmail.com>
2024-08-07 14:47:24 +0545

    feat: bool

---
+++ PHP DATATYPES/phpFloat.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * PHP Float
+ * A float (floating point number) is a number with a decimal point or a number in exponential form.
+ * In the following example $x is a float. The PHP var_dump() function returns the data type and value:
+ **/
+$x = 55.66;
+var_dump($x);
+?>
