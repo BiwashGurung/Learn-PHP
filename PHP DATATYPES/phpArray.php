00a66f7a485edd25d636b99ce0cd18fe376cbd69
BiwashGurung <varungrg10@gmail.com>
2024-08-07 14:50:28 +0545

    php array

---
+++ PHP DATATYPES/phpArray.php
@@ -0,0 +1,10 @@
+<?php
+/*
+PHP Array
+An array stores multiple values in one single variable.
+
+In the following example $cars is an array. The PHP var_dump() function returns the data type and value:
+ */
+$x = array("de","dex","dexy");
+var_dump($x);
+?>
--- PHP DATATYPES/phpBoolean.php
+++ PHP DATATYPES/phpBoolean.php
@@ -1,4 +1,5 @@
 <?php
 $x = false;
 var_dump($x);
+//Booleans are often used in conditional testing.
 ?>
