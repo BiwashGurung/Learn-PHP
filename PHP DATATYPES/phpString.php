145580d29c3c771b367a0041b3330836fff7972a
BiwashGurung <varungrg10@gmail.com>
2024-08-07 14:35:43 +0545

    feat: php string

---
+++ PHP DATATYPES/phpString.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ PHP String
+A string is a sequence of characters, like "Hello world!".
+
+A string can be any text inside quotes. You can use single or double quotes:
+ */
+$x = "Hello";
+$y = "world";
+
+var_dump($x);
+echo"<br>";
+var_dump($y);
+?>
