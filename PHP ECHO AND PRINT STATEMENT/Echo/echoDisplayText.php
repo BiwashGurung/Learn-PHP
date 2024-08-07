d728e5188b64903dda14e21f98d005e905d022d2
BiwashGurung <varungrg10@gmail.com>
2024-08-07 12:47:30 +0545

    feat: echoDisplayText

---
+++ PHP ECHO AND PRINT STATEMENT/Echo/echoDisplayText.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <h1>The following example shows how to output text with the echo command (notice that the text can contain HTML markup):</h1>
+    <body>
+       <?php
+       echo"<h1>This is heading 1</h1>";
+       echo"adding a br!<br>";
+       echo"Im learning PHP<br>";
+       echo"This " ,"is " ,"a " ,"string " ,"that" ,"is" ,"made" ,"with" ,"multiple","parameters";
+
+       ?> 
+    </body>
+</html>
