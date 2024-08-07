7b4433058a779aac2ea6ca5d91cd0fc1a879b6f3
BiwashGurung <varungrg10@gmail.com>
2024-08-07 13:12:42 +0545

    feat: echo single quote

--- PHP ECHO AND PRINT STATEMENT/Echo/echoUsingSingleQuotes.php
+++ PHP ECHO AND PRINT STATEMENT/Echo/echoUsingSingleQuotes.php
@@ -1,8 +1,20 @@
 <!DOCTYPE html>
 <html>
     <body>
-       <?php
-       
+        <h3>Using Single Quotes</h3>
+        <p>Strings are surrounded by quotes, but there is a difference between single and double quotes in PHP.
+
+When using double quotes, variables can be inserted to the string as in the example above.
+
+When using single quotes, variables have to be inserted using the . operator, like this:</p>
+        <?php
+           $name = "Muna";
+           $caste = "Pun";
+           $description = "Hello I am $name and I am from lekhnath";
+
+           echo '<h2>'. 'About ' . $name , $caste .'</h2>' ;
+           echo 'Description : ' . $description;
+
        ?> 
     </body>
 </html>
